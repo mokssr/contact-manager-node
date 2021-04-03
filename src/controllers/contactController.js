753105cc7dbc9f89cd626b56c720af9450dd4732
Sukma Raharja <sukmaraharja@outlook.co.id>
2021-04-04 03:21:41 +0800

    add contactService

--- src/controllers/contactController.js
+++ src/controllers/contactController.js
@@ -1,23 +1,33 @@
-const getContact = (req, res) => {
+const Contact = require('../models/contactModel')
+const contactService = require('../services/contactService')
+
+const getContact = async (req, res) => {
+  let contactList = await Contact.find()
   // if query params exist, search by name
   // make middleware for query params
-  res.json({ message: "get all contacts" })
+  res.json({ message: "get all contacts", content: contactList })
 }
 
-const getContactById = (req, res) => {
-  res.json({ message: `get contact with id = ${req.params.id}` })
+const getContactById = async (req, res) => {
+  let id = req.params.id
+  let result = await contactService.findById(id)
+  res.json({ message: `get contact with id = ${req.params.id}`, data: result })
 }
 
-const addContact = (req, res) => {
-  res.json({ message: "add new contact", content: req.body })
+const addContact = async (req, res) => {
+  let payload = { firstName, lastName, phone, email, address } = req.body
+  let result = await contactService.add(payload)
+  res.json({ message: "add new contact", content: result })
 }
 
 const updateContact = (req, res) => {
   res.json({ message: `update contact with id = ${req.params.id}`, content: req.body })
 }
 
-const deleteContact = (req, res) => {
-  res.json({ message: `deleting contact with id = ${req.params.id}` })
+const deleteContact = async (req, res) => {
+  let id = req.params.id
+  let result = await contactService.deleteById(id)
+  res.json({ message: `deleting contact with id = ${req.params.id}`, data: result })
 }
 
 module.exports = {
